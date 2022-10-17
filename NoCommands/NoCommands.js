ec5c6903706ccc90599754d2d8d645cbc9edc96a
jainampatel <jainampatel3971@gmail.com>
2022-10-17 19:08:02 +0530

    Added custom message

--- NoCommands/NoCommands.js
+++ NoCommands/NoCommands.js
@@ -3,55 +3,73 @@
 /// <reference path="../App.js" />
 
 (function () {
-  'use strict';
+  "use strict";
 
   // The initialize function must be run each time a new page is loaded
   Office.initialize = function (reason) {
-    $(document).ready( () => {
+    $(document).ready(() => {
       app.initialize();
 
-      $('#insertDefault').click(insertDefault);
-      $('#insertMsg1').click(insertMsg1);
-      $('#insertMsg2').click(insertMsg2);
-      $('#insertMsg3').click(insertMsg3);
-      $('#insertCustom').click(insertCustom);
+      $("#insertDefault").click(insertDefault);
+      $("#insertMsg1").click(insertMsg1);
+      $("#insertMsg2").click(insertMsg2);
+      $("#insertMsg3").click(insertMsg3);
+      $("#insertMsg4").click(insertMsg4);
+      $("#insertCustom").click(insertCustom);
     });
   };
-  
+
   function insertText(textToInsert) {
     // Insert as plain text (CoercionType.Text)
     Office.context.mailbox.item.body.setSelectedDataAsync(
-      textToInsert, 
-      { coercionType: Office.CoercionType.Text }, 
-       (asyncResult) => {
+      textToInsert,
+      { coercionType: Office.CoercionType.Text },
+      (asyncResult) => {
         // Display the result to the user
         if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
-          app.showNotification("Success", "\"" + textToInsert + "\" inserted successfully.");
+          app.showNotification(
+            "Success",
+            '"' + textToInsert + '" inserted successfully.'
+          );
+        } else {
+          app.showNotification(
+            "Error",
+            'Failed to insert "' +
+              textToInsert +
+              '": ' +
+              asyncResult.error.message
+          );
         }
-        else {
-          app.showNotification("Error", "Failed to insert \"" + textToInsert + "\": " + asyncResult.error.message);
-        }
-      });
+      }
+    );
   }
 
   function insertDefault() {
     insertText("Inserted by the Add-in Command Demo add-in.");
   }
-  
+
   function insertMsg1() {
     insertText("Hello World!");
   }
-  
+
   function insertMsg2() {
     insertText("Add-in commands are cool!");
   }
-  
+
   function insertMsg3() {
-    insertText("Visit https://developer.microsoft.com/en-us/outlook/ today for all of your add-in development needs.");
+    insertText(
+      "Visit https://developer.microsoft.com/en-us/outlook/ today for all of your add-in development needs."
+    );
   }
-  
+
+  function insertMsg4() {
+    insertText(
+      "Click on Hello World! or Add-ins are cool! to get the message."
+    );
+  }
+
   function insertCustom() {
-    const textToInsert = $('#textToInsert').val();
+    const textToInsert = $("#textToInsert").val();
     insertText(textToInsert);
   }
 })();
